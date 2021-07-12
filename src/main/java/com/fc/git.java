cb83297915820a14daf3407e52b808108fc09cf1
fc1104 <87295493+fc1104@users.noreply.github.com>
2021-07-12 23:36:30 +0800

    pull commit

--- src/main/java/com/fc/git.java
+++ src/main/java/com/fc/git.java
@@ -14,6 +14,7 @@ public class git {
         System.out.println("hot-fix......");
         System.out.println("push test");
         System.out.println("push test2");
+        System.out.println("pull test");
 
 
     }
