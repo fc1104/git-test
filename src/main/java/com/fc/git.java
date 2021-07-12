c92263155fc32605ba2ebe07227f9341f744f15e
fc1104 <1749212009@qq.com>
2021-07-12 22:42:01 +0800

    second commit

--- src/main/java/com/fc/git.java
+++ src/main/java/com/fc/git.java
@@ -6,6 +6,8 @@ package com.fc;
  */
 public class git {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+
     }
 }
