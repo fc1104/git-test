2f844b0d9b7b0d302d805b3754bf35191c6d2ba3
fc1104 <1749212009@qq.com>
2021-07-12 22:55:48 +0800

    master commit1

--- src/main/java/com/fc/git.java
+++ src/main/java/com/fc/git.java
@@ -10,6 +10,7 @@ public class git {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hot-fix test");
+        System.out.println("master......");
 
     }
 }
